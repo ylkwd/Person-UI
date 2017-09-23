4ae9ed0e9a3286a44454088fe802df0d09a7af44
ylkwd <yaoxuanluan@lyxs-15-Pro.local>
2017-09-22 20:16:43 -0500

    first

---
+++ Person-UI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Person-UI
+//
+//  Created by Yaoxuan Luan on 9/22/17.
+//  Copyright © 2017 Yaoxuan Luan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
